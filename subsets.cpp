2459a7aaa49444cf5418ac07911041f390493e8c
harpreet1025 <harpreet@chatimity.com>
2014-12-08 23:25:58 +0530

    Generates subsets using bitmask

---
+++ subsets.cpp
@@ -0,0 +1,27 @@
+/*
+** Generates every subset of the set {0, 1, 2, 3, 4, 5, ..., N-1}.
+*/
+#include<iostream>
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+
+int main() {
+    int N = 3;
+    for(int mask = 0; mask < (1<<N); mask++) {
+        vector<int> subset;
+        for(int val = 0; val < N; val++)
+            if(mask & (1 << val))
+                subset.push_back(val);
+
+        cout << "{";
+        for(int i=0; i < subset.size(); i++){
+            if(i)
+                cout << ", ";
+            cout << subset[i];
+        }
+        cout << "}\n";
+    }
+    return 0;
+}
